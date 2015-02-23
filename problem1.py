4ad689a717cd64608aa07ec477865d955cad209e
James Murphy <jmurphy4@mail.umw.edu>
2015-02-23 14:28:30 -0500

    Problem 1 solved.

---
+++ problem1.py
@@ -0,0 +1,17 @@
+import sys
+
+# If we list all the natural numbers below 10 that are multiples of 3 or 5,
+# we get 3, 5, 6, and 9. The sum of these multiples is 23. Find the sum of
+# all the multiples of 3 or 5 below 100.
+
+total = 0
+
+for i in range(1, 1000):
+	if i % 3 == 0:
+		total += i
+	elif i % 5 == 0:
+		total += i
+
+sys.stdout.write("The sum of multiples of 3 or 5 between 1-1000 is ")
+sys.stdout.write(str(total))
+sys.stdout.write("\n")
