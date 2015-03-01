a263251b3e621f2524cd7a23bb4efd5db23175a0
James Murphy <jmurphy4@mail.umw.edu>
2015-03-01 03:10:38 -0500

    Solved problem 25, compatibility for problem 2.

---
+++ problem25.py
@@ -0,0 +1,50 @@
+import sys
+
+# The Fibonacci sequence is defined by the recurrence relation:
+	# F_n = F_n-1 + F_n-2, where F_1 = 1 and F_2 = 1.
+
+# Hence the first twelve terms will be:
+	# F_1 = 1
+	# F_2 = 1
+	# F_3 = 2
+	# F_4 = 3
+	# F_5 = 5
+	# F_6 = 8
+	# F_7 = 13
+	# F_8 = 21
+	# F_9 = 34
+	# F_10 = 55
+	# F_11 = 89
+	# F_12 = 144
+
+# The 12th term, F_12, is the first term to contain three digits.
+# What is the first term in the Fibonacci sequence to contain 1000 digits?
+
+# Modify fibonacci( ) function borrowed from problem 2.
+def fibonacci(n):
+	if n <= 1:
+		return n
+	
+	curr = 1
+	prev = 1
+	
+	for i in xrange(1, n - 1):
+		temp = curr
+		curr += prev
+		prev = temp
+		
+	return curr
+
+term = 1
+found = False
+
+while found == False:
+	if len(str(fibonacci(term))) >= 1000:
+		found = True
+	else:
+		term += 1
+
+term = str(term)
+
+sys.stdout.write("The first term in the Fibonacci sequence to contain ")
+sys.stdout.write("1000 digits\nis term " + term + ".\n")
